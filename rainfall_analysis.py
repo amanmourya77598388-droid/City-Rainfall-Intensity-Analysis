1c22086b970b9ca2d04fce8bef6972925dac6c87
Aman Mourya <amanmourya77598388@gmail.com>
2025-12-26 10:57:25 +0530

    Update rainfall_analysis.py

--- rainfall_analysis.py
+++ rainfall_analysis.py
@@ -1,16 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-# ===============================
-# Step 1: Create Rainfall Dataset
-# ===============================
 np.random.seed(42)
 
 dates = pd.date_range(start="2024-01-01", periods=120)
 rainfall = np.random.gamma(shape=2, scale=6, size=120)
-
-# Extreme rainfall events
 rainfall[20] = 65
 rainfall[70] = 90
 rainfall[105] = 75
@@ -22,10 +16,6 @@ df = pd.DataFrame({
 
 print("Sample Data:")
 print(df.head())
-
-# ===============================
-# Step 2: Z-Score Calculation
-# ===============================
 mean = df["Rainfall_mm"].mean()
 std = df["Rainfall_mm"].std()
 
@@ -34,10 +24,6 @@ df["Extreme_Event"] = df["Z_score"].abs() > 2
 
 print("\nExtreme Rainfall Events:")
 print(df[df["Extreme_Event"]])
-
-# ===============================
-# Step 3: Plot Graph
-# ===============================
 plt.figure(figsize=(12,5))
 plt.plot(df["Date"], df["Rainfall_mm"], label="Rainfall Intensity")
 plt.scatter(
@@ -52,4 +38,5 @@ plt.title("City Rainfall Intensity Analysis")
 plt.legend()
 plt.xticks(rotation=45)
 plt.tight_layout()
+
 plt.show()
